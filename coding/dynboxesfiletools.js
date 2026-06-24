850fb12207e2fb085f534b4e980ca15edb04289c
FrostCat224 <dinao.dinim@gmail.com>
2026-06-24 14:03:49 +0200

    Create dynboxesfiletools.js

---
+++ coding/dynboxesfiletools.js
@@ -0,0 +1,25 @@
+
+const items = [
+    {
+        title: "Image Resizer",
+        image: "https://frostcat224.github.io/frostcatgameswebsite/img/unknown.png",
+        url: "https://frostcat224.github.io/frostcatgameswebsite/filetools/imageresizer.html",
+        size: "square"
+    },
+];
+
+const container = document.getElementById("dynamicBoxes");
+
+items.forEach(item => {
+    const box = document.createElement("a");
+
+    box.href = item.url;
+    box.className = `game-box ${item.size}`;
+
+    box.innerHTML = `
+        <img src="${item.image}" alt="${item.title}">
+        <div class="game-title">${item.title}</div>
+    `;
+
+    container.appendChild(box);
+});
